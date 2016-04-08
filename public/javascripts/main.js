ad1a9be9ef8e9e26a19cc9067ff82ff0a961e38a
Ryan Jones <ryandjones14@gmail.com>
2016-04-08 16:39:51 -0500

    working on js for color generator

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,34 @@
 $(function() {
-  $.('#start').on('click', function(){
-    this.parent().hide('#landing')
+// COLOR GENERATOR
+  function randomColor(){
+    var red = Math.floor(Math.random()*256);
+    var green = Math.floor(Math.random()*256);
+    var blue = Math.floor(Math.random()*256);
+    var color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
   }
+
+  var colors = [];
+
+  function randomColors(num){
+    for (var i=1; i<=num; i++){
+      var color = randomColor();
+      colors.push(color);
+    }
+    return colors;
+  }
+
+  $('#start').on('click', function(){
+    $(this).parent().hide();
+    $('#generator').css('display', 'block');
+    $('#color1').css()
+  });
+
+  $('h1').on('click', function(){
+    $('#generator').css('display', 'none');
+    $('#landing').show();
+  });
+
+//  FONTS GENERATOR
+
 });
