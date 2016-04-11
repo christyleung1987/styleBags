f3d04d0e107aa6fa3bd81575fd49ed33c11c5c6c
Lauren Zugai <lauren@zugai.com>
2016-04-11 12:29:29 -0500

    Still working on FB Auth, almost there

--- models/user.js
+++ models/user.js
@@ -3,9 +3,10 @@ var bcrypt   = require('bcrypt-nodejs');
 
 var userSchema = new mongoose.Schema({
   local : {
-    //username: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true, unique: true}
+    //username: {unique: true},
+    email: {type: String, unique: true},
+    password: {type: String},
+    facebookId: {type: Number, unique: true}
   }
 });
 
@@ -16,6 +17,22 @@ userSchema.statics.hash = function(password){
 userSchema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.local.password);
 }
+
+userSchema.statics.findOrCreate = function findOrCreate(profile, cb){
+  var userObj = new this();
+  this.findOne({_id : profile.id},function(err,result){
+    if(!result) {
+      console.log(profile);
+      console.log(profile.facebookId);
+      userObj.facebookId = profile.facebookId;
+      userObj.save(cb);
+      console.log(userObj);
+    } else {
+      console.log("#2")
+      cb(err,result);
+    }
+  });
+};
 
 var User = mongoose.model('User', userSchema);
 
