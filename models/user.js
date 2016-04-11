7c26cbd42a7799e700d900a232198f363f6a6e41
Lauren Zugai <lauren@zugai.com>
2016-04-11 17:26:58 -0500

    Changed facebook to local in userschema

--- models/user.js
+++ models/user.js
@@ -5,12 +5,8 @@ var userSchema = new mongoose.Schema({
   local : {
     //username: {unique: true},
     email: {type: String},
-<<<<<<< HEAD
-    password: {type: String}
-=======
     password: {type: String},
     facebookId: {type: Number}
->>>>>>> 0c3bf39e0570eb4e706f0925f16e2f36ed148a14
   }
 
 });
@@ -25,9 +21,9 @@ userSchema.methods.validPassword = function(password) {
 
 userSchema.statics.findOrCreate = function findOrCreate(profile, cb){
   var userObj = new this();
-  this.findOne({'facebook.facebookId' : profile.facebookId},function(err,result){
+  this.findOne({'local.facebookId' : profile.facebookId},function(err,result){
     if(!result) {
-      userObj.facebook.facebookId = profile.facebookId;
+      userObj.local.facebookId = profile.facebookId;
       userObj.save(cb);
     } else {
       cb(err,result);
