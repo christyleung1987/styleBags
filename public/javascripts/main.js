32876ab7bd442c28a1d6fd5883e06ca81199d9b3
Ryan Jones <ryandjones14@gmail.com>
2016-04-11 12:28:56 -0500

    shows correct number of color divs based on user input

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -21,18 +21,23 @@ $(function() {
     return colors;
   }
 
-  // COLOR Generator jQuery
-
-  $('#start').on('click', function(){
-    $(this).parent().hide();
+  function colorsGenerator(){
     randomColors(num);
-    $('#generator').css('display', 'block');
     $('#color1').css('background-color', `${colors[0]}`);
     $('#color2').css('background-color', `${colors[1]}`);
     $('#color3').css('background-color', `${colors[2]}`);
     $('#color4').css('background-color', `${colors[3]}`);
     $('#color5').css('background-color', `${colors[4]}`);
     $('#color6').css('background-color', `${colors[5]}`);
+  }
+
+  // COLOR Generator jQuery
+
+  $('#start').on('click', function(){
+    $(this).parent().hide();
+    $('#generator').css('display', 'block');
+    colors=[];
+    colorsGenerator();
   });
 
   $('h1').on('click', function(){
@@ -40,13 +45,15 @@ $(function() {
     $('#landing').show();
   });
 
-  //C & DOWN, F & DOWN KEYPRESSES
+  // //C & DOWN, F & DOWN KEYPRESSES
   var keys = {67: false, 70: false, 40: false};
   $(document).keydown(function(e) {
     if (e.keyCode in keys) {
       keys[e.keyCode] = true;
       if (keys[40] && keys[67]) {
         console.log("c and down arrow pressed");
+        colors = [];
+        colorsGenerator();
       } else if (keys[40] && keys[70]) {
         console.log("f and down arrow pressed");
       }
@@ -54,6 +61,40 @@ $(function() {
   }).keyup(function(e) {
     if (e.keyCode in keys) {
       keys[e.keyCode] = false;
+    }
+  });
+
+  // change ammount of color divs shown based on user input
+  $('#color-ammt').on('click', function(){
+    console.log($('#color-number').val());
+    colors = [];
+    colorsGenerator();
+    if ($('#color-number').val() < 4) {
+      $('#color6').hide();
+      $('#color5').hide();
+      $('#color4').hide();
+      colors.pop();
+      colors.pop();
+      colors.pop();
+      console.log(colors);
+    } else if ($('#color-number').val() < 5) {
+      $('#color6').hide();
+      $('#color5').hide();
+      $('#color4').show();
+      colors.pop();
+      colors.pop();
+      console.log(colors);
+    } else if ($('#color-number').val() < 6) {
+      $('#color6').hide();
+      $('#color5').show();
+      $('#color4').show();
+      colors.pop();
+      console.log(colors);
+    } else {
+      $('#color6').show();
+      $('#color5').show();
+      $('#color4').show();
+      console.log(colors);
     }
   });
 
