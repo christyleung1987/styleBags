1d4306dfa967b062004887c12ff9ea941d4d1c53
Lauren Zugai <lauren@zugai.com>
2016-04-12 14:43:03 -0500

    Working on displaying colorbags from user

--- models/colorBag.js
+++ models/colorBag.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var colorBagSchema = new mongoose.Schema({
   name: {type: String, required: true},
   rgbs: Array,
+  date: {type: Date, default: Date.now},
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
--- models/font.js
+++ models/font.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var fontSchema = new mongoose.Schema({
   fontName: {type: String, required: true, unique: true},
   category: {type: String},
+  date: {type: Date, default: Date.now},
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
